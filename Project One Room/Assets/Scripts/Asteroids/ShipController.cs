1e0a6114ae8cdfd39abf3896b2ee62e68297c7b2
Quinn Nolan <quinn.nolan@students.plymouth.ac.uk>
2016-12-11 04:02:32 +0000

    Added movement to asteroids, started to add animations to donkey kong

---
+++ Project One Room/Assets/Scripts/Asteroids/ShipController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipController : MonoBehaviour {
+
+    public float speed = 5.0f;
+    Vector2 movement;
+    Rigidbody2D rigidbody2D;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    //FixedUpdate processes physics every frame
+    //x and y get values from wasd keys for movement
+    //this is then used to calculate players velocity which is times by public speed so its easier to manipulate in unity
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Move(x, y);
+
+    }
+
+    void Move(float x, float y)
+    {
+        movement.Set(x, y);
+        //movement = movement.normalized;
+        rigidbody2D.velocity = movement * speed;
+        rigidbody2D.angularVelocity = 0.0f;
+    }
+}
