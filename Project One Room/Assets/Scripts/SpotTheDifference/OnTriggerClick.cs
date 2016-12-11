23dac51ece1a33d3b0a69ff1fda8ad3f3bb81b22
Quinn Nolan <quinn.nolan@students.plymouth.ac.uk>
2016-12-11 05:13:05 +0000

    Started spot the difference scene.

---
+++ Project One Room/Assets/Scripts/SpotTheDifference/OnTriggerClick.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnTriggerClick : MonoBehaviour {
+
+    public GameObject circleHighlights;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        print("click recieved");
+        target.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    void Reveal()
+    {
+        circleHighlights.SetActive(true);
+    }
+
+}
