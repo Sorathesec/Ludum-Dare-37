56c55828124e4643bd57bf9f47034a5261236689
Jacob Williams <squalljacob@homtail.co.uk>
2016-12-13 00:56:31 +0000

    Limited input on the spot the difference game to every 0.5 seconds to stop red flicker when getting a correct answer

--- Project One Room/Assets/Scripts/SpotTheDifference/OnTriggerClick.cs
+++ Project One Room/Assets/Scripts/SpotTheDifference/OnTriggerClick.cs
@@ -8,17 +8,16 @@ namespace LudumDare37
     public class OnTriggerClick : MonoBehaviour
     {
         [SerializeField]
+        private SpriteRenderer rewardImage;
+
+        [SerializeField]
+        private SpriteRenderer[] displayImages;
+
+        [SerializeField]
         private int fearPenaltyValue = 3;
 
         [SerializeField]
         private int fearRemovalValue = 20;
-
-        [SerializeField]
-        private GameObject rewardImage;
-
-        [SerializeField]
-        private GameObject[] displayImages;
-
 
         private static float diminishingReturns = 1.0f;
 
@@ -30,6 +29,7 @@ namespace LudumDare37
         [SerializeField]
         private SpriteRenderer errorImage;
         int printOnce = 0;
+        bool canClick = true;
 
         void Start()
         {
@@ -56,16 +56,17 @@ namespace LudumDare37
 
         void OnTriggerStay2D(Collider2D target)
         {
-
-            if (target.CompareTag("Shootable"))
+            if (canClick && target.CompareTag("Shootable"))
             {
-                if (target.GetComponent<Animator>() != null && target.GetComponent<Animator>().enabled != true)
+                SpriteRenderer targetSprite = target.gameObject.GetComponent<SpriteRenderer>();
+
+                if (targetSprite != null)
                 {
-                    target.GetComponent<Animator>().enabled = true;
+                    StartCoroutine(FadeInMarker(targetSprite));
                     circlesHighlighted++;
                     Win();
+                    Invoke("ResetClick", 0.5f);
                 }
-
             }
             else
             {
@@ -78,71 +79,96 @@ namespace LudumDare37
                     }
                 }
 
-                if (!haveValid)
+                if (!haveValid && target.GetComponent<Animator>() != null)
                 {
-                    if (target.GetComponent<Animator>() != null)
+                    wrongAnimator.enabled = true;
+                    target.GetComponent<Animator>().Play("Miss");
+                    Wrong.GetComponent<SpriteRenderer>().enabled = true;
+                    Invoke("ResetAnimation", 0.755f);
+
+                    if (printOnce < 1)
                     {
-
-                        wrongAnimator.enabled = true;
-                        target.GetComponent<Animator>().Play("Miss");
-                        Wrong.GetComponent<SpriteRenderer>().enabled = true;
-                        Invoke("ResetAnimation", 0.755f);
-
-                        if (printOnce < 1)
-                        {
-                            print("Miss");
-                            Miss();
-                            printOnce = 2;
-                            Invoke("ResetPrintOnce", 1.0f);
-                        }
-
+                        Miss();
+                        printOnce = 2;
+                        Invoke("ResetPrintOnce", 1.0f);
                     }
                 }
             }
         }
-        
-        void Win()
+
+        void ResetClick()
         {
+            canClick = true;
+        }
+
+        IEnumerator FadeInMarker(SpriteRenderer marker)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Color newColor = marker.color;
+                newColor.a += 0.02f;
+                marker.color = newColor;
+
+                yield return new WaitForSeconds(0.02f);
+            }
+        }
+
+        private void Win()
+        {
+            canClick = false;
             if (circlesHighlighted >= 5)
             {
                 int fearReduction = (int)(fearRemovalValue / diminishingReturns);
+
                 if (fearReduction < 1)
                 {
                     fearReduction = 1;
                 }
-
                 FearController.instance.RemoveFear(fearReduction);
-
                 diminishingReturns = diminishingReturns / 3;
 
-                DisplayVictoryScreen();
+                StartCoroutine(FadeInSingle());
             }
         }
 
-        void DisplayVictoryScreen()
+        IEnumerator FadeInSingle()
         {
-            for(int i = 0; i < displayImages.Length; i++)
+            for (int i = 0; i < 100; i++)
             {
-                displayImages[i].SetActive(false);
+                Color newColor = rewardImage.color;
+                newColor.a += 0.01f;
+                rewardImage.color = newColor;
+
+                yield return new WaitForSeconds(0.01f);
             }
-
-            rewardImage.SetActive(true);
-
-            Invoke("ReturnToMenu", 5.0f);
+            StartCoroutine(FadeOutDouble());
         }
 
+        IEnumerator FadeOutDouble()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < displayImages.Length; j++)
+                {
+                    Color newColor = displayImages[j].color;
+                    newColor.a -= 0.01f;
+                    displayImages[j].color = newColor;
+                }
+                yield return new WaitForSeconds(0.01f);
+            }
+            Invoke("ReturnToMenu", 3.0f);
+        }
 
         void ReturnToMenu()
         {
             DisableSpotTheDifference.willDisable = true;
-
             FadeMusic.instance.switchToRoom();
-
             SceneManager.LoadScene("Main");
         }
 
         void Miss()
         {
+            canClick = false;
             FearController.instance.AddFear(fearPenaltyValue);
         }
 
